82b383804cb9b5c3f346f3590c0542430f49b3f0
sanyaminkin <sanyaminkin@gmail.com>
2021-07-11 01:29:46 +0300

    Main.java original added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    long bonus = service.calculate(amount, registered);
-    System.out.println(bonus);
+    service.calculate(amount, registered);
+
 
   }
 }
